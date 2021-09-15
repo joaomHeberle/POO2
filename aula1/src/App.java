a8b7110cb0029d19c6af58bd011e2e022a31242a
Joao Heberle <joaomheberle@gmail.com>
2021-09-15 10:58:05 -0300

    att

--- aula1/src/App.java
+++ aula1/src/App.java
@@ -5,13 +5,14 @@ import java.nio.file.Paths;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Path dir1= Paths.get("aula1","aula1","dados");
+        Path dir1= Paths.get("aula1");
         
-        // Path dirNovo = dir1.resolve("dados");
-        // if(! Files.exists(dirNovo)){
-        //     Files.createDirectories(dirNovo);
-        // }
-        Path arquivo = dir1.resolve("ideias.csv");
+        Path dirNovo = dir1.resolve("dados");
+        if(! Files.exists(dirNovo)){
+            Files.createDirectories(dirNovo);
+        }
+        Path dirArquivo = Paths.get("aula1", "dados");
+        Path arquivo = dirArquivo.resolve("ideias.csv");
         if(! Files.exists(arquivo)){
             Files.createFile(arquivo);
         }
